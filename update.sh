3f9dba29f97ba29a5dc1bddad8323d9db77307bb
dgamboaestrada <dgamboaestrada@gmail.com>
2015-09-26 17:31:22 -0500

    Add file update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git fetch origin master
+git pull origin master
+
+echo "Updating..."
+
+echo "Copy .vimrc"
+cp vimrc ~/.vimrc
+
+echo "Install plugins"
+vim +PluginInstall +qall
+echo "Clean plugins"
+vim +PluginClean +qall
+
+echo "----- Successfully updated ------"
