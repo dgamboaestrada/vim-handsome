ccbe64879ee38f9093e7b44e76538d341fbf10b6
dgamboaestrada <dgamboaestrada@gmail.com>
2015-10-03 18:22:34 -0500

    Add osx install script.

---
+++ osx-install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Installing vim awesome..."
+
+echo "Copy .vimrc"
+cp ./vim/vimrc ~/.vimrc
+
+if [ ! -d ~/.vim/bundle/Vundle.vim/ ]
+then
+    echo "Copy .vim/"
+    if [ ! -d ~/.vim/ ]
+    then
+        mkdir ~/.vim/
+    fi
+    if [ ! -d ~/.vim/bundle/ ]
+    then
+        mkdir ~/.vim/bundle/
+    fi
+    cp -R ./vim/bundle/ ~/.vim/
+fi
+
+echo "Copy colors"
+if [ ! -d ~/.vim/colors/ ]
+then
+    mkdir ~/.vim/colors/
+fi
+cp -R ./vim/vim-colors-solarized/colors/ ~/.vim/
+
+echo "Install plugins"
+vim +PluginInstall +qall
+echo "Clean plugins"
+vim +PluginClean +qall
+
+echo "Install powerline fonts"
+source ./other/fonts/install.sh
+
+echo "Copy .tmux.config"
+if [ ! -f ~/.tmux.config ]
+then
+    cp ./other/tmux.config ~/.tmux.config
+else
+    sed -i '' '/# ----- Begin vim-awesome -----/,/# ----- End vim-awesome -----/ d' ~/.tmux.config
+    cat ./other/tmux.config >> ~/.tmux.config
+fi
+
+echo "Copy .zshrc"
+if [ ! -f ~/.zshrc ]
+then
+    cp ./other/zshrc ~/.zshrc
+else
+    sed -i '' '/# ----- Begin vim-awesome -----/,/# ----- End vim-awesome -----/ d' ~/.zshrc
+    cat ./other/zshrc >> ~/.zshrc
+fi
+
+echo "----- Successfully installed ------"
